58591b65358d447f826776e90e9d5715b13c1a99
Jesper15 <230717@edu.rocfriesepoort.nl>
2022-09-08 15:23:04 +0200

    Adding an array and array loop in twig.

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,11 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class AlbumController {
+class AlbumController extends AbstractController {
+
+    #[Route('/')]
+    public function homepage(): Response {
+
+        $tracks = [
+            ['song' => 'Come around again', 'artist' => 'Armin van Buuren'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('albums/index.html.twig', [
+            'title' => 'Songs',
+            'tracks' => $tracks,
+        ]);
+    }
+
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response {
 

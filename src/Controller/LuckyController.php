ce995d09df9942f95ca0cfce6f2b38f8837bcdda
Jesper15 <230717@edu.rocfriesepoort.nl>
2022-09-08 11:22:11 +0200

    Adding template and new code.

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,16 +2,18 @@
 // src src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController {
+class LuckyController extends AbstractController {
     #[Route('/lucky/number')]
     public function number(): Response {
         $number = random_int(1, 100);
 
-        return new Response(
-            '<html><body>new number='.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
